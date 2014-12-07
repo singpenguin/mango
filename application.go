ea63f498fc879d95d3baa4e77ecf202b4247d2b2
zhenjing <zhenjinglee@gmail.com>
2014-12-07 21:01:43 +0800

    add SetSecureCookie func

--- application.go
+++ application.go
@@ -1,45 +1,45 @@
 package mango
 
 import (
-	"net/http"
-	"fmt"
-	"runtime"
-	"os"
-	"os/signal"
+    "net/http"
+    "fmt"
+    "runtime"
+    "os"
+    "os/signal"
 )
 
 var (
-	Debug = false
-	CookieSecret = "fLjUfxqXtfNoIldA0A0J"
+    Debug = false
+    CookieSecret = "fLjUfxqXtfNoIldA0A0J"
 )
 
 type Application struct {
-	Addr string
-	Port int
-	Url map[string]interface{}
-	StaticPath string
-	TemplatePath string
+    Addr string
+    Port int
+    Url map[string]interface{}
+    StaticPath string
+    TemplatePath string
 }
 
 func (app *Application) Run() {
-	runtime.GOMAXPROCS(runtime.NumCPU())
-	http.Handle("/",NewRouter(app.Url))
-	if (app.StaticPath != "") {
-		http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir(app.StaticPath))))
-	}
-	if (app.TemplatePath != "") {
-		TemplateLoader(app.TemplatePath)
-	}
+    runtime.GOMAXPROCS(runtime.NumCPU())
+    http.Handle("/",NewRouter(app.Url))
+    if (app.StaticPath != "") {
+        http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir(app.StaticPath))))
+    }
+    if (app.TemplatePath != "") {
+        TemplateLoader(app.TemplatePath)
+    }
 
-	fmt.Printf("http://%s:%d\n", app.Addr, app.Port)
+    fmt.Printf("http://%s:%d\n", app.Addr, app.Port)
 
-	go func() {
-		err := http.ListenAndServe(fmt.Sprintf("%s:%d", app.Addr, app.Port), nil)
-		if err != nil {
-			fmt.Println("Failed to start server:", err)
-		}
-	}()
-	ch := make(chan os.Signal)
-	signal.Notify(ch, os.Interrupt, os.Kill)
-	<-ch
+    go func() {
+        err := http.ListenAndServe(fmt.Sprintf("%s:%d", app.Addr, app.Port), nil)
+        if err != nil {
+            fmt.Println("Failed to start server:", err)
+        }
+    }()
+    ch := make(chan os.Signal)
+    signal.Notify(ch, os.Interrupt, os.Kill)
+    <-ch
 }
--- template.go
+++ template.go
@@ -1,59 +1,59 @@
 package mango
 
 import (
-	"html/template"
-	"path/filepath"
-	"os"
-	"io/ioutil"
-	"strings"
-	"fmt"
-	"regexp"
+    "html/template"
+    "path/filepath"
+    "os"
+    "io/ioutil"
+    "strings"
+    "fmt"
+    "regexp"
 )
 
 
 var Template map[string]*template.Template
 
 func TemplateLoader(basepath string) map[string]*template.Template{
-	Template = make(map[string]*template.Template)
-	tmp := make(map[string]string)
+    Template = make(map[string]*template.Template)
+    tmp := make(map[string]string)
 
-	err := filepath.Walk(basepath, func(path string, f os.FileInfo, err error) error {
-		if ( f == nil ) {return err}
-		if f.IsDir() {return nil}
-		if strings.HasPrefix(f.Name(), ".") {return nil}
+    err := filepath.Walk(basepath, func(path string, f os.FileInfo, err error) error {
+        if ( f == nil ) {return err}
+        if f.IsDir() {return nil}
+        if strings.HasPrefix(f.Name(), ".") {return nil}
 
-		fileBytes, e := ioutil.ReadFile(path)
-		if e != nil {
-			fmt.Println("Failed reading file:", path)
-			return nil
-		}
-		tmp[path[len(basepath)+1:]] = string(fileBytes)
+        fileBytes, e := ioutil.ReadFile(path)
+        if e != nil {
+            fmt.Println("Failed reading file:", path)
+            return nil
+        }
+        tmp[path[len(basepath)+1:]] = string(fileBytes)
 
-		return nil
-	})
-	if err != nil {
-		fmt.Println("filepath.Walk() returned ", err)
-	}
+        return nil
+    })
+    if err != nil {
+        fmt.Println("filepath.Walk() returned ", err)
+    }
 
-	re, _ := regexp.Compile("\\{\\{[\\S\\s]+?\\}\\}")
-	for k, _ := range tmp {
-		m := re.FindAllString(tmp[k], -1)
-		if len(m) > 0 {
-			for j, _ := range m {
-				s := strings.Trim(m[j], "{")
-				s = strings.Trim(s, "}")
-				s = strings.TrimSpace(s)
-				if s == k {
-					fmt.Println("Template Circular reference:", k)
-					break
-				}
-				tmp[k] = strings.Replace(tmp[k], m[j], tmp[s], -1)
-			}
-		}
-		Template[k], err = template.New(k).Parse(tmp[k])
-		if err != nil {
-			fmt.Println("Failed parse template:", k, err)
-		}
-	}
-	return Template
+    re, _ := regexp.Compile("\\{\\{[\\S\\s]+?\\}\\}")
+    for k, _ := range tmp {
+        m := re.FindAllString(tmp[k], -1)
+        if len(m) > 0 {
+            for j, _ := range m {
+                s := strings.Trim(m[j], "{")
+                s = strings.Trim(s, "}")
+                s = strings.TrimSpace(s)
+                if s == k {
+                    fmt.Println("Template Circular reference:", k)
+                    break
+                }
+                tmp[k] = strings.Replace(tmp[k], m[j], tmp[s], -1)
+            }
+        }
+        Template[k], err = template.New(k).Parse(tmp[k])
+        if err != nil {
+            fmt.Println("Failed parse template:", k, err)
+        }
+    }
+    return Template
 }
