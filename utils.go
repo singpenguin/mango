2576377860c4f8500675f4bf52eec3d93e9338bd
zhenjing <zhenjinglee@gmail.com>
2015-03-11 11:41:36 +0800

    watch html file in debug mode

--- utils.go
+++ utils.go
@@ -1,51 +1,51 @@
 package mango
 
 import (
-    "time"
-    "strconv"
-    "fmt"
-    "encoding/base64"
-    "crypto/hmac"
-    "crypto/sha256"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+	"strconv"
+	"time"
 )
 
 func Timestamp() int64 {
-    return time.Now().Unix()
+	return time.Now().Unix()
 }
 
 func FormatInt(i int64, base ...interface{}) string {
-    var b int
-    if len(base) == 1 {
-        b = base[0].(int)
-    } else {
-        b = 10
-    }
-    return strconv.FormatInt(i, b)
+	var b int
+	if len(base) == 1 {
+		b = base[0].(int)
+	} else {
+		b = 10
+	}
+	return strconv.FormatInt(i, b)
 }
 
 func ParseInt(s string) (int64, error) {
-    return strconv.ParseInt(s, 10, 64)
+	return strconv.ParseInt(s, 10, 64)
 }
 
 func Base64Encode(src []byte) string {
-    rs := base64.StdEncoding.EncodeToString(src)
-    return rs
+	rs := base64.StdEncoding.EncodeToString(src)
+	return rs
 }
 
 func Base64Decode(src string) (string, error) {
-    rs, err := base64.StdEncoding.DecodeString(src)
-    return string(rs), err
+	rs, err := base64.StdEncoding.DecodeString(src)
+	return string(rs), err
 }
 
 func HmacSha256(message string, key string) string {
-    mac := hmac.New(sha256.New, []byte(key))
-    mac.Write([]byte(message))
-    exmac := mac.Sum(nil)
-    return fmt.Sprintf("%x", exmac)
+	mac := hmac.New(sha256.New, []byte(key))
+	mac.Write([]byte(message))
+	exmac := mac.Sum(nil)
+	return fmt.Sprintf("%x", exmac)
 }
 
-func unpack(s []string, vars... *string) {
-    for i, str := range s {
-        *vars[i] = str
-    }
+func unpack(s []string, vars ...*string) {
+	for i, str := range s {
+		*vars[i] = str
+	}
 }
--- web.go
+++ web.go
@@ -1,322 +1,322 @@
 package mango
 
 import (
-    "net/http"
-    "fmt"
-    "strings"
-    "io/ioutil"
-    "bytes"
-    "mime/multipart"
-    "net/url"
-    "errors"
+	"bytes"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"mime/multipart"
+	"net/http"
+	"net/url"
+	"strings"
 )
 
 type HTTPRequest struct {
-    Args []string        //route params
-    Method string        //HTTP method
-    RemoteAddr string    //ip
-    Path string
-    Host string
-    Params url.Values    //GET or POST params
-    Body []byte            //Content-Type=application/octet-stream, Body=http body
-    Files map[string][]*multipart.FileHeader        //Content-Type=multipart/form-data
+	Args       []string //route params
+	Method     string   //HTTP method
+	RemoteAddr string   //ip
+	Path       string
+	Host       string
+	Params     url.Values                         //GET or POST params
+	Body       []byte                             //Content-Type=application/octet-stream, Body=http body
+	Files      map[string][]*multipart.FileHeader //Content-Type=multipart/form-data
 
-    W http.ResponseWriter
-    R *http.Request
+	W http.ResponseWriter
+	R *http.Request
 }
 
 func (self *HTTPRequest) Init(w http.ResponseWriter, r *http.Request, a []string) {
-    self.W = w
-    self.R = r
-    self.Method = r.Method
-    self.Path = r.URL.Path
-    self.Args = a
-    self.Host = r.Host
+	self.W = w
+	self.R = r
+	self.Method = r.Method
+	self.Path = r.URL.Path
+	self.Args = a
+	self.Host = r.Host
 
-    ip := r.Header.Get("X-Forwarded-For")
-    if ip == "" {
-        ip = r.RemoteAddr
-    }
-    self.RemoteAddr = strings.Split(ip, ":")[0]
-    self.parseBodyArguments()
+	ip := r.Header.Get("X-Forwarded-For")
+	if ip == "" {
+		ip = r.RemoteAddr
+	}
+	self.RemoteAddr = strings.Split(ip, ":")[0]
+	self.parseBodyArguments()
 }
 
 func (self *HTTPRequest) UserAgent() string {
-    return self.R.Header.Get("User-Agent")
+	return self.R.Header.Get("User-Agent")
 }
 
 func (self *HTTPRequest) Header(s string) string {
-    return self.R.Header.Get(s)
+	return self.R.Header.Get(s)
 }
 
 func (self *HTTPRequest) SetHeader(k, v string) {
-    self.W.Header().Set(k, v)
+	self.W.Header().Set(k, v)
 }
 
 func (self *HTTPRequest) SetStatus(st int) {
-    self.W.WriteHeader(st)
+	self.W.WriteHeader(st)
 }
 
-func (self *HTTPRequest) Cookie(k string) (string,error) {
-    ck, err := self.R.Cookie(k)
-    if err != nil {
-        return "", err
-    }
-    return ck.Value, nil
+func (self *HTTPRequest) Cookie(k string) (string, error) {
+	ck, err := self.R.Cookie(k)
+	if err != nil {
+		return "", err
+	}
+	return ck.Value, nil
 }
 
 var cks = strings.NewReplacer("\n", "-", "\r", "-")
 var cvs = strings.NewReplacer("\n", " ", "\r", " ", ";", " ")
 
 func (self *HTTPRequest) SetSecureCookie(k, v string, params ...interface{}) {
-    bv := []byte(v)
-    ts := FormatInt(Timestamp())
-    v = Base64Encode(bv)
-    sa := []string{}
-    sa = append(sa, "2|1:0")
-    sa = append(sa, fmt.Sprintf("%d:%s", len(ts), ts))
-    sa = append(sa, fmt.Sprintf("%d:%s", len(k), k))
-    sa = append(sa, fmt.Sprintf("%d:%s", len(v), v))
-    sa = append(sa, "")
-    to_sign := strings.Join(sa, "|")
-    signature := HmacSha256(to_sign, CookieSecret)
+	bv := []byte(v)
+	ts := FormatInt(Timestamp())
+	v = Base64Encode(bv)
+	sa := []string{}
+	sa = append(sa, "2|1:0")
+	sa = append(sa, fmt.Sprintf("%d:%s", len(ts), ts))
+	sa = append(sa, fmt.Sprintf("%d:%s", len(k), k))
+	sa = append(sa, fmt.Sprintf("%d:%s", len(v), v))
+	sa = append(sa, "")
+	to_sign := strings.Join(sa, "|")
+	signature := HmacSha256(to_sign, CookieSecret)
 
-    self.SetCookie(k, to_sign+signature, params...)
+	self.SetCookie(k, to_sign+signature, params...)
 }
 
-func (self *HTTPRequest) GetSecureCookie(k string) (string,error) {
-    value, err := self.Cookie(k)
-    if err != nil || len(value) < 64 {
-        return "", err
-    }
-    _rest := value[2:]
-    _, rest, err := consumeField(_rest)
-    if err != nil {
-        return "", err
-    }
-    timestamp_field, rest, err := consumeField(rest)
-    if err != nil {
-        return "", err
-    }
-    name_field, rest, err := consumeField(rest)
-    if err != nil {
-        return "", err
-    }
-    value_field, rest, err := consumeField(rest)
-    if err != nil {
-        return "", err
-    }
-    passed_sig := rest
-    signed_str := value[:len(value)-len(passed_sig)]
-    signature := HmacSha256(signed_str, CookieSecret)
-    if passed_sig != signature {
-        return "", errors.New("signature invalid")
-    }
-    if name_field != k {
-        return "", errors.New("cookie name invalid")
-    }
-    timestamp, _ := ParseInt(timestamp_field)
-    if timestamp < Timestamp() - 2678400 {
-        return "", errors.New("signature has expired")
-    }
-    devalue, err := Base64Decode(value_field)
-    if err != nil {
-        return "", errors.New("value cannot be decode")
-    }
-    return devalue, nil
+func (self *HTTPRequest) GetSecureCookie(k string) (string, error) {
+	value, err := self.Cookie(k)
+	if err != nil || len(value) < 64 {
+		return "", err
+	}
+	_rest := value[2:]
+	_, rest, err := consumeField(_rest)
+	if err != nil {
+		return "", err
+	}
+	timestamp_field, rest, err := consumeField(rest)
+	if err != nil {
+		return "", err
+	}
+	name_field, rest, err := consumeField(rest)
+	if err != nil {
+		return "", err
+	}
+	value_field, rest, err := consumeField(rest)
+	if err != nil {
+		return "", err
+	}
+	passed_sig := rest
+	signed_str := value[:len(value)-len(passed_sig)]
+	signature := HmacSha256(signed_str, CookieSecret)
+	if passed_sig != signature {
+		return "", errors.New("signature invalid")
+	}
+	if name_field != k {
+		return "", errors.New("cookie name invalid")
+	}
+	timestamp, _ := ParseInt(timestamp_field)
+	if timestamp < Timestamp()-2678400 {
+		return "", errors.New("signature has expired")
+	}
+	devalue, err := Base64Decode(value_field)
+	if err != nil {
+		return "", errors.New("value cannot be decode")
+	}
+	return devalue, nil
 }
 
-func consumeField(value string) (string,string,error) {
-    var length, rest string
-    unpack(strings.SplitN(value, ":", 2), &length, &rest)
+func consumeField(value string) (string, string, error) {
+	var length, rest string
+	unpack(strings.SplitN(value, ":", 2), &length, &rest)
 
-    n, err := ParseInt(length)
-    if err != nil {
-        return "", "", errors.New("length field invalid")
-    }
-    key_field := rest[:n]
-    if rest[n:n+1] != "|" {
-        return "", "", errors.New("malformed signed value")
-    }
-    rest = rest[n+1:]
-    return key_field, rest, nil
+	n, err := ParseInt(length)
+	if err != nil {
+		return "", "", errors.New("length field invalid")
+	}
+	key_field := rest[:n]
+	if rest[n:n+1] != "|" {
+		return "", "", errors.New("malformed signed value")
+	}
+	rest = rest[n+1:]
+	return key_field, rest, nil
 }
 
 func (self *HTTPRequest) SetCookie(k, v string, params ...interface{}) {
-    var b bytes.Buffer
-    fmt.Fprintf(&b, "%s=%s", cks.Replace(k), cvs.Replace(v))
-    if len(params) > 0 {
-        switch v := params[0].(type) {
-        case int:
-            if v > 0 {
-                fmt.Fprintf(&b, "; Max-Age=%d", v)
-            } else if v < 0 {
-                fmt.Fprintf(&b, "; Max-Age=0")
-            }
-        case int64:
-            if v > 0 {
-                fmt.Fprintf(&b, "; Max-Age=%d", v)
-            } else if v < 0 {
-                fmt.Fprintf(&b, "; Max-Age=0")
-            }
-        case int32:
-            if v > 0 {
-                fmt.Fprintf(&b, "; Max-Age=%d", v)
-            } else if v < 0 {
-                fmt.Fprintf(&b, "; Max-Age=0")
-            }
-        }
-    }
-    // Path, Domain, Secure, HttpOnly
+	var b bytes.Buffer
+	fmt.Fprintf(&b, "%s=%s", cks.Replace(k), cvs.Replace(v))
+	if len(params) > 0 {
+		switch v := params[0].(type) {
+		case int:
+			if v > 0 {
+				fmt.Fprintf(&b, "; Max-Age=%d", v)
+			} else if v < 0 {
+				fmt.Fprintf(&b, "; Max-Age=0")
+			}
+		case int64:
+			if v > 0 {
+				fmt.Fprintf(&b, "; Max-Age=%d", v)
+			} else if v < 0 {
+				fmt.Fprintf(&b, "; Max-Age=0")
+			}
+		case int32:
+			if v > 0 {
+				fmt.Fprintf(&b, "; Max-Age=%d", v)
+			} else if v < 0 {
+				fmt.Fprintf(&b, "; Max-Age=0")
+			}
+		}
+	}
+	// Path, Domain, Secure, HttpOnly
 
-    // default "/"
-    if len(params) > 1 {
-        if v, ok := params[1].(string); ok && len(v) > 0 {
-            fmt.Fprintf(&b, "; Path=%s", cvs.Replace(v))
-        }
-    } else {
-        fmt.Fprintf(&b, "; Path=%s", "/")
-    }
+	// default "/"
+	if len(params) > 1 {
+		if v, ok := params[1].(string); ok && len(v) > 0 {
+			fmt.Fprintf(&b, "; Path=%s", cvs.Replace(v))
+		}
+	} else {
+		fmt.Fprintf(&b, "; Path=%s", "/")
+	}
 
-    // default empty
-    if len(params) > 2 {
-        if v, ok := params[2].(string); ok && len(v) > 0 {
-            fmt.Fprintf(&b, "; Domain=%s", cvs.Replace(v))
-        }
-    }
+	// default empty
+	if len(params) > 2 {
+		if v, ok := params[2].(string); ok && len(v) > 0 {
+			fmt.Fprintf(&b, "; Domain=%s", cvs.Replace(v))
+		}
+	}
 
-    // default empty
-    if len(params) > 3 {
-        var secure bool
-        switch v := params[3].(type) {
-        case bool:
-            secure = v
-        default:
-            if params[3] != nil {
-                secure = true
-            }
-        }
-        if secure {
-            fmt.Fprintf(&b, "; Secure")
-        }
-    }
+	// default empty
+	if len(params) > 3 {
+		var secure bool
+		switch v := params[3].(type) {
+		case bool:
+			secure = v
+		default:
+			if params[3] != nil {
+				secure = true
+			}
+		}
+		if secure {
+			fmt.Fprintf(&b, "; Secure")
+		}
+	}
 
-    // default false. 
-    httponly := false
-    if len(params) > 4 {
-        if v, ok := params[4].(bool); ok && v {
-            // HttpOnly = true
-            httponly = true
-        }
-    }
+	// default false.
+	httponly := false
+	if len(params) > 4 {
+		if v, ok := params[4].(bool); ok && v {
+			// HttpOnly = true
+			httponly = true
+		}
+	}
 
-    if httponly {
-        fmt.Fprintf(&b, "; HttpOnly")
-    }
+	if httponly {
+		fmt.Fprintf(&b, "; HttpOnly")
+	}
 
-    self.W.Header().Add("Set-Cookie", b.String())
+	self.W.Header().Add("Set-Cookie", b.String())
 }
 
 func (self *HTTPRequest) Scheme() string {
-    if self.R.URL.Scheme != "" {
-        return self.R.URL.Scheme
-    } else if self.R.TLS == nil {
-        return "http"
-    } else {
-        return "https"
-    }
+	if self.R.URL.Scheme != "" {
+		return self.R.URL.Scheme
+	} else if self.R.TLS == nil {
+		return "http"
+	} else {
+		return "https"
+	}
 }
 
 func (self *HTTPRequest) Redirect(newloc string) {
-    self.SetHeader("Location", newloc)
-    self.SetStatus(303)
+	self.SetHeader("Location", newloc)
+	self.SetStatus(303)
 }
 
 func (self *HTTPRequest) Query() map[string][]string {
-    return self.R.URL.Query()
+	return self.R.URL.Query()
 }
 
 func (self *HTTPRequest) QueryString() string {
-    if s := strings.Split(self.R.RequestURI, "?"); len(s) > 1 {
-        return s[1]
-    }else{
-        return ""
-    }
+	if s := strings.Split(self.R.RequestURI, "?"); len(s) > 1 {
+		return s[1]
+	} else {
+		return ""
+	}
 }
 
 func (self *HTTPRequest) parseBodyArguments() {
-    qs := self.R.URL.Query()
-    var pt url.Values
-    ct := self.Header("Content-Type")
-    if strings.HasPrefix(ct, "application/x-www-form-urlencoded") {
-        if err := self.R.ParseForm(); err != nil {
-            fmt.Println("Error parsing request body:", err)
-        } else {
-            pt = self.R.Form
-        }
-    } else if strings.HasPrefix(ct, "multipart/form-data") {
-        // TODO: Extract the multipart form param so app can set it.
-        if err := self.R.ParseMultipartForm(32 << 20 /* 32 MB */); err != nil {
-            fmt.Println("Error parsing request body:", err)
-        } else {
-            self.Files = self.R.MultipartForm.File
-        }
-    } else if strings.HasPrefix(ct, "application/octet-stream") {
-        self.Body = self.Data()
-    }
+	qs := self.R.URL.Query()
+	var pt url.Values
+	ct := self.Header("Content-Type")
+	if strings.HasPrefix(ct, "application/x-www-form-urlencoded") {
+		if err := self.R.ParseForm(); err != nil {
+			fmt.Println("Error parsing request body:", err)
+		} else {
+			pt = self.R.Form
+		}
+	} else if strings.HasPrefix(ct, "multipart/form-data") {
+		// TODO: Extract the multipart form param so app can set it.
+		if err := self.R.ParseMultipartForm(32 << 20 /* 32 MB */); err != nil {
+			fmt.Println("Error parsing request body:", err)
+		} else {
+			self.Files = self.R.MultipartForm.File
+		}
+	} else if strings.HasPrefix(ct, "application/octet-stream") {
+		self.Body = self.Data()
+	}
 
-    num := len(qs) + len(pt)
+	num := len(qs) + len(pt)
 
-    if num == 0 {
-        self.Params = make(url.Values, 0)
-        return
-    }
-    values := make(url.Values, num)
-    for k, v := range qs {
-        values[k] = append(values[k], v...)
-    }
-    for k, v := range pt {
-        values[k] = append(values[k], v...)
-    }
-    self.Params = values
+	if num == 0 {
+		self.Params = make(url.Values, 0)
+		return
+	}
+	values := make(url.Values, num)
+	for k, v := range qs {
+		values[k] = append(values[k], v...)
+	}
+	for k, v := range pt {
+		values[k] = append(values[k], v...)
+	}
+	self.Params = values
 }
 
 func (self *HTTPRequest) Data() []byte {
-    body, _ := ioutil.ReadAll(self.R.Body)
-    self.R.Body.Close()
-    bf := bytes.NewBuffer(body)
-    self.R.Body = ioutil.NopCloser(bf)
-    return body
+	body, _ := ioutil.ReadAll(self.R.Body)
+	self.R.Body.Close()
+	bf := bytes.NewBuffer(body)
+	self.R.Body = ioutil.NopCloser(bf)
+	return body
 }
 
 func (self *HTTPRequest) Write(s string) {
-    self.W.Write([]byte(s))
+	self.W.Write([]byte(s))
 }
 
 func (self *HTTPRequest) Render(name string, data interface{}) {
-    Template[name].Execute(self.W, data)
+	Template[name].Execute(self.W, data)
 }
 
 func (self *HTTPRequest) GET() {
-    http.Error(self.W, "Method GET Not Allowed", 405)
+	http.Error(self.W, "Method GET Not Allowed", 405)
 }
 
 func (self *HTTPRequest) POST() {
-    http.Error(self.W, "Method POST Not Allowed", 405)
+	http.Error(self.W, "Method POST Not Allowed", 405)
 }
 
 func (self *HTTPRequest) HEAD() {
-    http.Error(self.W, "Method HEAD Not Allowed", 405)
+	http.Error(self.W, "Method HEAD Not Allowed", 405)
 }
 
 func (self *HTTPRequest) OPTION() {
-    http.Error(self.W, "Method OPTION Not Allowed", 405)
+	http.Error(self.W, "Method OPTION Not Allowed", 405)
 }
 
 func (self *HTTPRequest) PUT() {
-    http.Error(self.W, "Method PUT Not Allowed", 405)
+	http.Error(self.W, "Method PUT Not Allowed", 405)
 }
 
 func (self *HTTPRequest) DELETE() {
-    http.Error(self.W, "Method DELETE Not Allowed", 405)
+	http.Error(self.W, "Method DELETE Not Allowed", 405)
 }
